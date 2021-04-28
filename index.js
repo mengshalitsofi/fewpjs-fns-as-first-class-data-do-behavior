29846433d1610a66e8e8884ce8fc3eddb18dfb62
mengshalitsofi <mengshalitsofi@gmail.com>
2021-04-27 19:06:20 -0700

    commit tree a54714f4aedb6e84fd0ced2b81b1e2c0df808e4f

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const h = parseInt(time.split(':')[0])
+  if (h < 12) {
+    return "Good Morning"
+  } else if (h < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(str) {
+  const element = document.getElementById("greeting");
+  element.innerText = str;
+}
